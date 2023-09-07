3a970c22e946be373d7c5276a52e5b614540c6ec
Clae <56447997+clayruh@users.noreply.github.com>
2023-09-07 16:18:13 -0400

    add ball class

--- lib/classes/ball.py
+++ lib/classes/ball.py
@@ -3,15 +3,17 @@ import pygame
 class Ball(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        original_ball = pygame.image.load("./assets/images/red-ball.png")
-        scaled_width = original_ball.get_width() // 7
-        scaled_height = original_ball.get_height() // 7
-        self.image = pygame.transform.scale(original_ball, (scaled_width, scaled_height))
+        self.image = pygame.image.load("./assets/images/red-ball.png")
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
+        self.visible = True
 
     def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        if self.visible:
+            screen.blit(self.image, self.rect)
 
     def update(self):
         pass
+
+    def hide(self):
+        self.visible = False
